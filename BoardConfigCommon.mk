7a3096f174b826e4bbec73c74c47668c175f8b11
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-04-06 10:02:31 +0800

    Update BoardConfigCommon.mk

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -239,4 +239,4 @@ WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # Include the proprietary files BoardConfig.
-include vendor/xiaomi/sm8550-common/BoardConfigVendor.mk
+#include vendor/xiaomi/sm8550-common/BoardConfigVendor.mk
